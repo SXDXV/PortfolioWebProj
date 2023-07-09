df6f5649d0fd1c17928173df338f70804a1e3344
Dmitry <shevtsov.dima.03@gmail.com>
2023-07-09 19:32:52 +0300

    Add project files.

---
+++ PortfolioWebProj/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioWebProj.Models;
+using System.Diagnostics;
+
+namespace PortfolioWebProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult Design()
+        {
+            return View();
+        }
+
+        public IActionResult Programming()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
